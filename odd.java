1960a7d4855e99d5556e0af65eca15189b576d99
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-13 06:35:53 -0700

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class odd
+{
+public static void main(String argS[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter te no");
+a=sc.nextInt();
+if(a%2==0)
+{
+System.out.println("even");
+}
+else if(a<0)
+{
+System.out.println(" invalid");
+}
+else{
+System.out.println("odd");
+}
+}}
