165fe96b1e0c78e6e515941bb25931dddd7a6a3d
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-13 06:42:44 -0700

    Create lar.java

---
+++ lar.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class lar
+{
+public static void main(String argS[])
+{
+int a,b,c;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the no");
+a=sc.nextInt();
+System.out.println("enter the no");
+b=sc.nextInt();
+System.out.println("enter the no");
+c=sc.nextInt();
+
+
+if((a>b)&&(a>c))
+{
+System.out.println(""+a);
+}
+else if(b>c)
+{
+System.out.println(""+b);
+}
+else{
+System.out.println(""+c);
+}
+}}
