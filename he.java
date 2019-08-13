7f1823535c88b40ae687426df259ae6027c6a554
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-13 06:42:08 -0700

    Create he.java

---
+++ he.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class he
+{
+public static void main(String argS[])
+{
+int a,b;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the no");
+a=sc.nextInt();
+for(int i=0;i<=a;i++)
+{
+System.out.println("hello");
+}
+}}
