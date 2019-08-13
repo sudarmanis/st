922e744b730ec97199a2c2de63667e78d2d6b15d
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-13 06:31:22 -0700

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class pos
+{
+public static void main(String argS[])
+{
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter te no");
+a=sc.nextInt();
+if(a<0)
+{
+System.out.println("negative");
+}
+else if(a==0)
+{
+System.out.println(" zero");
+}
+else{
+System.out.println("positive");
+}
+}}
