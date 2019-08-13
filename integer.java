1b521e2e07db055d9735fa58e2693509f65771dd
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-13 07:13:35 -0700

    Create integer.java

---
+++ integer.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+public class integer
+{
+	public static void main(String[] args) {
+		int n,k,m=0,i;
+		Scanner sc=new Scanner(System.in);
+	    System.out.println("value");
+	    n=sc.nextInt();
+	    System.out.println("value2");
+	    k=sc.nextInt();
+	    for( i=1;i<=n;i++)
+	    {
+	        System.out.println("value n ="+i);
+	    }
+	    for( i=1;i<=k;i++)
+	    {
+	        m=m+i;
+	        System.out.println("values of k="+m);
+	        
+	    }
+	}
+}
