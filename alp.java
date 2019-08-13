796a0e591a687a23935da0d2842fbce910b7137a
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-13 06:40:05 -0700

    Create alp.java

---
+++ alp.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class alp
+{
+public static void main(String argS[])
+{
+char a;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the char");
+a=sc.next().charAt(0);
+if((a>='a')&&(a<='z'))
+{
+System.out.println("alphabet");
+}
+else{
+System.out.println("no");
+}
+}}
