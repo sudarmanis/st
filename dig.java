fdf66bac8278f0f37db1ca2b26cf4fe9122950f7
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-13 07:03:33 -0700

    Create dig.java

---
+++ dig.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class dig
+{
+public static void main(String argS[])
+{
+int a, n,r=0;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the no");
+a=sc.nextInt();
+for(n=1;n<=a;n++)
+{
+r=r+n;
+System.out.println(""+r);
+}
+}
+}
+
