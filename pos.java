100ee5d9dc84027a4ff786124e9d4984eb597248
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-28 07:18:40 -0700

    Update pos.java

--- pos.java
+++ pos.java
@@ -5,16 +5,12 @@ public static void main(String argS[])
 {
 int a;
 Scanner sc=new Scanner(System.in);
-System.out.println("enter te no");
 a=sc.nextInt();
 if(a<0)
 {
 System.out.println("negative");
 }
-else if(a==0)
-{
-System.out.println(" zero");
-}
+
 else{
 System.out.println("positive");
 }
